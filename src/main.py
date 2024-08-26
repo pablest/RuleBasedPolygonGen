eb604ace1de080a0235ff4502fa84b56df4af996
pablest <pverde195@gmail.com>
2024-08-27 00:57:32 +0200

    Added user input

--- src/main.py
+++ src/main.py
@@ -10,14 +10,46 @@ def generate_rules_name(rules):
     rules_text += ''.join(map(str, rules))
     return rules_text
 
+def rules_str_to_int_duples(rules_string):
+    # input_string is a strin like this turnx,multiplicationx turny,multiplicationy Example:2,2 3,2
+    # Split the input string into individual rules
+    individual_rules = rules_string.split()
+    rules = []
+
+    # Iterate over each operation in the list
+    for individual_rule in individual_rules:
+        turn_str, multiplication_str = individual_rule.split(',')
+
+        turn = int(turn_str)
+        multiplication = int(multiplication_str)
+
+        # Append the tuple (turn, multiplication) to the rules list
+        rules.append((turn, multiplication))
+
+    return rules
+
+
+#Variables to draw the new polygon
 rules = [(2,0),(4,5)] #rules are defined as an array of tuples formed like (turn,multiplication)
 polygon_sides = 5
 show_construction = True
-pts = generate_fig.generate_increased_polygon_fig(polygon_sides,rules) #points of the poligon, the result is an numpy array of arrays of x and y coordinates
+change_colors = True
+
+#inputs to modify the variables to draw the new polygon. If None is entered, then it will use the values of the code
+first_input = input("Enter the number of sides of the regular polygon you want to modify ")
+if(first_input != ""):
+    polygon_sides = int(first_input)
+    rules_str = input("Enter the rules of the constructed polygon in pairs turnx,multiplicationx turny,multiplicationy Example:2,2 3,2\n") 
+    rules = rules_str_to_int_duples(rules_str)
+    show_construction = input("Do you want to see the process of construction of the polygon? y/n ").lower() == 'y'
+    if(show_construction):
+        change_colors = input("Do you want sides of the polygon to change color when it is being built? y/n ").lower() == 'y'
+
+pts = generate_fig.generate_increased_polygon_fig(polygon_sides,rules) #points of the polygon, the result is an numpy array of arrays of x and y coordinates
 fig_show = show_figure.FigureShower(fig_name = str(polygon_sides),rules_name = generate_rules_name(rules))
 
 if(show_construction):
-    fig_show.show_figure_construction(pts)
+    fig_show.show_figure_construction(pts,change_colors)
 else:
     fig_show.show_figure(pts)
 
--- src/show_figure.py
+++ src/show_figure.py
@@ -3,7 +3,14 @@ import numpy as np
 import keyboard
 from PIL import Image
 import os
+
+#constants to control the quality of the drawing
+LINE_QUALITY = cv2.LINE_4
+LINE_SIZE = 2
+MARGIN = 1/6
+
 class FigureShower:
+
     def __init__(self,fig_name = "N_sides",rules_name = "Turn_Multiplication_(N_N)"):
         self.fig_name = fig_name
         self.rules_name = rules_name
@@ -16,7 +23,8 @@ class FigureShower:
         cv2.destroyAllWindows()
 
     def operate_image(self,img,turn = None):
-        #cualquier tecla avanzar
+        #Controls: 
+        #any key to get next drawing
         #q quit
         #s save fig 
         while True:
@@ -40,47 +48,49 @@ class FigureShower:
 
     def show_figure(self,pts):
 
-        #define the quality of drawing variables
-        line_quality = cv2.LINE_4
-        line_size = 2
-        margin = 1/6
-
         #first we center the points to the corner
         pts -= pts.min(axis = 0)
-        pts += np.int32(margin*pts.max(axis = 0))
+        pts += np.int32(MARGIN*pts.max(axis = 0))
 
         #then the image that have to cover all points
-        img = np.zeros((np.int32(pts[:,1].max()*(1+margin)),np.int32(pts[:,0].max()*(1+margin)),3), np.uint8)
+        img = np.zeros((np.int32(pts[:,1].max()*(1+MARGIN)),np.int32(pts[:,0].max()*(1+MARGIN)),3), np.uint8)
 
-        #show image
-        
+        #construct the polygon
         cv2.circle(img, pts[0], radius=3, color=(255, 255, 255), thickness=-1)
-        cv2.polylines(img,np.array([pts]),False,(0,255,255),line_size,line_quality)
+        cv2.polylines(img,np.array([pts]),False,(0,255,255),LINE_SIZE,LINE_QUALITY)
+
+        #show the image
+        print("Figure will be generated in another window\nPress q to quit, s to save the figure, and any other key to draw the next side")
         self.show_and_destroy_image(img)
         
-    def show_figure_construction(self,pts):
-
-        #define the quality of drawing variables
-        line_quality = cv2.LINE_4
-        line_size = 2
-        margin = 1/6
+    def show_figure_construction(self,pts,change_colors):
 
         #first we center the points to the corner
         pts -= pts.min(axis = 0)
-        pts += np.int32(margin*pts.max(axis = 0))
+        pts += np.int32(MARGIN*pts.max(axis = 0))
 
         #then the image that have to cover all points
-        img = np.zeros((np.int32(pts[:,1].max()*(1+margin)),np.int32(pts[:,0].max()*(1+margin)),3), np.uint8)
+        img = np.zeros((np.int32(pts[:,1].max()*(1+MARGIN)),np.int32(pts[:,0].max()*(1+MARGIN)),3), np.uint8)
 
         #show construction
         cv2.circle(img, pts[0], radius=3, color=(255, 255, 255), thickness=-1)
         last_pt = pts[0]
+        print("Figure will be generated in another window\nPress q to quit, s to save the figure, and any other key to draw the next side")
+
         for turn in range(1,pts.shape[0]):
-            cv2.polylines(img,np.array([[last_pt,pts[turn]]]),False,(turn*10%255,255,255),line_size,line_quality)
+            #draw the next side
+            if(change_colors):
+                cv2.polylines(img,np.array([[last_pt,pts[turn]]]),False,(turn*10%255,255,255),LINE_SIZE,LINE_QUALITY)
+            else:
+                cv2.polylines(img,np.array([[last_pt,pts[turn]]]),False,(0,255,255),LINE_SIZE,LINE_QUALITY)
+
+            #Let the user do whateverer it wants
             cv2.putText(img,f"Turn {turn}", (25,25),cv2.FONT_HERSHEY_SIMPLEX,1, (255,255,255), 1, cv2.LINE_AA)
             cv2.imshow('Figure', img)
             self.operate_image(img,f"Turn_{turn}")
             last_pt = pts[turn]
+
             #after showing, we delete the TURN text
             cv2.putText(img,f"Turn {turn}", (25,25),cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,0), 1, cv2.LINE_AA)
+
         self.show_and_destroy_image(img,"Final_turn")
